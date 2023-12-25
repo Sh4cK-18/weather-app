6a334d916fca95467d52b0c73f238a28c08187e7
Sh4cK-Dev <xwh3nver751@gmail.com>
2023-12-25 17:06:06 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="bg-gradient-to-r from-blue-900 to-purple-900 w-full h-screen">
+    <Weather />
+  </div>
+</template>
+
+<script setup>
+import Weather from './components/weather.vue'
+</script>
